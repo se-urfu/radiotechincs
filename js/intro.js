878fbd8910cfa4ffaa64fc81121c2722b3cf3f09
sc222 <v.a.shaternikov@gmail.com>
2018-11-28 15:34:29 +0500

    urfu image fix

--- js/intro.js
+++ js/intro.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){   
     var screenHeight = screen.height + "px";  
     console.log(screenHeight);
+    if(screen.height>1050)
+        screenHeight="1050px";
     document.getElementById ("intro").style.minHeight = screenHeight;
     //intro page must be fullscreen!
 });
