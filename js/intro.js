c8cf6fca22015e5194b3dca5cd5324ba8c37aa4f
sc222 <v.a.shaternikov@gmail.com>
2019-01-11 23:21:29 +0500

    updated urfu and graduates, cards equal height

--- js/intro.js
+++ js/intro.js
@@ -1,9 +1,8 @@
 $(document).ready(function(){   
     var screenHeight = screen.height + "px";  
     console.log(screenHeight);
-    if(screen.height>1050)
-        screenHeight="1050px";
+    if(screen.height>900)
+        screenHeight="900px";
     document.getElementById ("intro").style.minHeight = screenHeight;
     //intro page must be fullscreen!
 });
-
